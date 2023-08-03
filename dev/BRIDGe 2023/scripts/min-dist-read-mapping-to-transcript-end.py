56bab7c919f8feae063edfe2b739f488ec5f868d
nquintanaparrilla <139363152+nquintanaparrilla@users.noreply.github.com>
2023-08-02 20:58:41 -0400

    Delete dev directory

--- dev/BRIDGe 2023/scripts/min-dist-read-mapping-to-transcript-end.py
+++
@@ -1,7 +0,0 @@
-# for TLEN \=\= 0 and REF !\= *, get minimum distance of the read mapping site to the transcript end position of the same chromosome((
-
-from gtfparse import read_gtf
-
-df = read_gtf("indices/refdata-gex-GRCh38-2020-A/genes/genes.gtf")
-df_transcripts = df[df["feature"] == "transcript"]
-# print(head(df_transcripts))
